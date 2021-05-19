f6bf6f973ac447d65f71a33679f7003243af392b
L29 <linuztri@gmail.com>
2021-05-19 21:55:42 +0700

    fixing

--- api/api.py
+++ api/api.py
@@ -3,14 +3,9 @@ from shop import *
 from form import *
 from admin import *
 from flask_cors import CORS
-<<<<<<< HEAD
-=======
 
 CORS(app)
 
->>>>>>> 0626cb75eb4ed6df61be3cb55387677701626a0c
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-CORS(app)
