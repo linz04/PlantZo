ce4b3ad712e209e4d0c0d12ff794ffdef34d48fe
Ubuntu <plantzo@plantzo.5snr2ripirkuvi0jmf4e3sbb1b.ix.internal.cloudapp.net>
2021-06-13 19:04:05 +0000

    update docker

--- api/db.py
+++ api/db.py
@@ -1,7 +1,11 @@
 from app import app
+import sys
 import mysql.connector
 
 def MysqlConnection():
 	return (mysql.connector.connect(host="mysql", user="root", password="PlantZo@123", auth_plugin='mysql_native_password', database="plantzo"))
 
-mysql = MysqlConnection()
+try:
+        mysql = MysqlConnection()
+except:
+        sys.exit("error")
